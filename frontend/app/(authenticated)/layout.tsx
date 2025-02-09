9f99d13285188c4ce50444fb8ee437dadadc9d91
gasarvis <gasarvis@ncsu.edu>
2025-02-09 13:14:07 -0500

    Navbar text highliting when on a link

--- frontend/app/(authenticated)/layout.tsx
+++ frontend/app/(authenticated)/layout.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Test({children,}: Readonly<{children: React.ReactNode;}>) {
+  
+  const isActive = (path: string) => usePathname() === path;
+  
   return (
     <>
       <nav className="bg-gray-800 p-4">
@@ -11,11 +17,27 @@ export default function Test({children,}: Readonly<{children: React.ReactNode;}>
               VolunteerQuest
             </div>
           </Link>
-          <div className="space-x-4">
-            <Link href="/quests" className="text-white hover:text-gray-400">Quests</Link>
-            <Link href="/story" className="text-white hover:text-gray-400">Story</Link>
-            <Link href="/rewards" className="text-white hover:text-gray-400">Rewards</Link>
-            <Link href="/profile" className="text-white hover:text-gray-400">Profile</Link>
+          <div className="flex space-x-4">
+            <Link href="/quests">
+              <div className={isActive('/quests') ? 'text-primary hover:text-primary-dark' : 'text-white hover:text-gray-400'}>
+                Quests
+              </div>
+            </Link>
+            <Link href="/story">
+              <div className={isActive('/story') ? 'text-primary hover:text-primary-dark' : 'text-white hover:text-gray-400'}>
+                Story
+              </div>
+            </Link>
+            <Link href="/rewards">
+              <div className={isActive('/rewards') ? 'text-primary hover:text-primary-dark' : 'text-white hover:text-gray-400'}>
+                Rewards
+              </div>
+            </Link>
+            <Link href="/profile">
+              <div className={isActive('/profile') ? 'text-primary hover:text-primary-dark' : 'text-white hover:text-gray-400'}>
+                Profile
+              </div>
+            </Link>
           </div>
         </div>
       </nav>
